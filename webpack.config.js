253fbae2c2bb8cb115d190f3b31384242b71d280
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-12-10 08:03:31 +0200

    Adding ToDoList page structre

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  mode: 'development', //production
-  entry: { 
-    main: path.resolve(__dirname, 'src/app.js')
+  mode: 'development', // production
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    clean: true
+    clean: true,
   },
   devtool: 'inline-source-map',
-  devServer: {static: './dist'},
-  //loaders
+  devServer: { static: './dist' },
+  // loaders
   module: {
     rules: [
-      {test: /\.css$/, use: ['style-loader','css-loader']},
-      {test: /\.(svg|jpeg|ico|png|webp|jpg|gif)$/ , type: 'asset/resource'},
-      {test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource'},
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(svg|jpeg|ico|png|webp|jpg|gif)$/, type: 'asset/resource' },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource' },
+    ],
   },
-  //plugins
+  // plugins
   plugins: [new HtmlWebpackPlugin({
-    title: 'demo',//what is the title of your page
-    filename: 'index.html',// what is your file name
-    template: path.resolve(__dirname, 'src/temp.html')
-    })],
-}
+    title: 'Let Us Organize Your Day :)', // what is the title of your page
+    filename: 'index.html', // what is your file name
+    template: path.resolve(__dirname, 'src/index.html'),
+  })],
+};
